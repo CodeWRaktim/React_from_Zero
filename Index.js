2f9586ed37650f2a4d78623482f17b29901ed7a9
Raktim <ghoshrabi967@gmail.com>
2025-09-24 02:13:29 +0530

    lol

---
+++ Index.js
@@ -0,0 +1,5 @@
+console.log('Hello, world!');
+console.log('Welcome to learning JavaScript!');
+console.log('This is your Index.js file.');
+console.log('Let\'s write some more code!');
+console.log('Have a great day!');
