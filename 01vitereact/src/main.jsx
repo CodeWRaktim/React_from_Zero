ccbc6b8570beef37608f3861303427223e68c8f0
Raktim <ghoshrabi967@gmail.com>
2026-01-04 18:43:43 +0530

    REady_go

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+function MyApp(){
+   return(
+    <div>
+      <h1>Hello World</h1>
+    </div>
+   )
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MyApp />
+    <App/>
   </React.StrictMode>,
 )
---
+++ first_APP/src/components/card.jsx
@@ -0,0 +1,8 @@
+function Card(){
+    return <div className="red">
+        <h1>This is a Card Component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dignissimos.</p>
+    </div>;
+}
+
+export default Card;
